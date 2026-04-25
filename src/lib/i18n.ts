58d220e81d2a98ea3db8cf711fedd0f9d0a3d236
Markus Reinhold <leaffan@gmx.net>
2026-04-25 02:35:45 +0200

    Add and adjust more categories and translations

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -57,50 +57,90 @@ export type Translations = {
 
 export type CategoryKey =
 	| '20_goal'
-	| '30_assist'
 	| '50_point'
 	| '20_g_season'
+	| '30_g_season'
+	| '5_g_season_po'
+	| '30_a_season'
 	| '20_pts_season'
 	| '40_pts_season'
 	| '50_pts_season'
 	| '60_pts_season'
 	| '70_pts_season'
 	| '1_so_season'
+	| '5_so_season'
+	| '1_so_season_po'
 	| '100_gp_career'
 	| '500_gp_career'
+	| '10_gp_career_po'
+	| '25_gp_career_po'
+	| '50_gp_career_po'
+	| '100_gp_career_po'
+	| '50_g_career'
 	| '100_g_career'
 	| '150_g_career'
 	| '200_g_career'
+	| '10_g_career_po'
 	| '100_a_career'
 	| '200_a_career'
 	| '250_a_career'
 	| '250_pts_career'
 	| '400_pts_career'
 	| '500_pts_career'
+	| '30_pts_career_po'
 	| '500_pim_career'
+	| '100_pim_career_po'
+	| '50_pim_career_po'
 	| '10_so_career'
+	| '25_so_career'
 	| '1_teams'
 	| '2_teams'
+	| '3_teams'
 	| '3_plus_teams'
 	| '5_plus_teams'
+	| '6_teams'
 	| 'italy-champion'
 	| 'del-champion'
+	| 'del2-champion'
+	| 'dnl-champion'
 	| 'spengler-cup-winner'
+	| 'hobey-baker-memorial-award-finalist'
 	| '100_pim_season'
 	| '1994'
 	| '2025'
 	| 'WJC-20'
 	| 'WJC-20_MEDAL'
-	| 'WJC-20_GOLD_MEDAL'
+	| 'u20-wjc-gold-medal'
+	| 'world-championship-medal'
+	| 'shl-sm-silver-medal'
 	| 'drafted'
-	| 'not_drafted';
+	| 'not_drafted'
+	| 'dual_nationalities'
+	| 'draft_2000s'
+	| 'draft_round_1'
+	| 'draft_round_2'
+	| 'goalie'
+	| 'defender'
+	| 'forward'
+	| 'le_2.5_gaa_season'
+	| 'le_100_gp_career'
+	| 'le_10_gp_career'
+	| 'goalscorer_over_40'
+	| 'relative_del'
+	| 'relative_nhl'
+	| '50_w_career'
+	| '10_w_season_po'
+	| 'le_3.0_gaa_season_po'
+	;
 
 export const categories: Record<Language, Record<CategoryKey, string>> = {
 	de: {
 		'20_goal': '20-Tore-Saison',
-		'30_assist': '30-Vorlagen-Saison',
 		'50_point': '50-Punkte-Saison',
 		'20_g_season': '20-Tore-Saison',
+		'30_g_season': '30-Tore-Saison',
+		'5_g_season_po': '5-Tore-Saison (Playoffs)',
+		'30_a_season': '30-Vorlagen-Saison',
 		'20_pts_season': '20-Punkte-Saison',
 		'40_pts_season': '40-Punkte-Saison',
 		'50_pts_season': '50-Punkte-Saison',
@@ -108,39 +148,76 @@ export const categories: Record<Language, Record<CategoryKey, string>> = {
 		'70_pts_season': '70-Punkte-Saison',
 		'100_pim_season': '100 Strafminuten-Saison',
 		'1_so_season': '1+ Shutout-Saison',
+		'1_so_season_po': '1+ Shutout-Saison (Playoffs)',
+		'5_so_season': '5+ Shutout-Saison',
+		'le_2.5_gaa_season': 'Saison mit ≤ 2,50 GTS',
 		'100_gp_career': '100 Spiele / Karriere',
 		'500_gp_career': '500 Spiele / Karriere',
+		'10_gp_career_po': '10 Playoff-Spiele / Karriere',
+		'25_gp_career_po': '25 Playoff-Spiele / Karriere',
+		'50_gp_career_po': '50 Playoff-Spiele / Karriere',
+		'100_gp_career_po': '100 Playoff-Spiele / Karriere',
+		'50_g_career': '50 Tore / Karriere',
 		'100_g_career': '100 Tore / Karriere',
 		'150_g_career': '150 Tore / Karriere',
 		'200_g_career': '200 Tore / Karriere',
+		'10_g_career_po': '10 Playoff-Tore / Karriere',
 		'100_a_career': '100 Vorlagen / Karriere',
 		'200_a_career': '200 Vorlagen / Karriere',
 		'250_a_career': '250 Vorlagen / Karriere',
 		'250_pts_career': '250 Punkte / Karriere',
 		'400_pts_career': '400 Punkte / Karriere',
 		'500_pts_career': '500 Punkte / Karriere',
+		'30_pts_career_po': '30 Playoff-Punkte / Karriere',
 		'500_pim_career': '500 Strafminuten / Karriere',
+		'100_pim_career_po': '100 Strafminuten in Playoffs / Karriere',
+		'50_pim_career_po': '50 Strafminuten in Playoffs / Karriere',
 		'10_so_career': '10+ Shutouts / Karriere',
+		'25_so_career': '25+ Shutouts / Karriere',
+		'le_100_gp_career': '≤ 100 Spiele / Karriere',
+		'le_10_gp_career': '≤ 10 Spiele / Karriere',
 		'1_teams': 'Nur ein Team',
 		'2_teams': 'Exakt 2 Teams',
 		'3_plus_teams': '3+ Teams',
+		'3_teams': 'Exakt 3 Teams',
 		'5_plus_teams': '5+ Teams',
+		'6_teams': 'Exakt 6 Teams',
 		'del-champion': 'DEL-Meister',
+		'del2-champion': 'DEL2-Meister',
+		'dnl-champion': 'DNL-Meister',
 		'italy-champion': 'Italienischer Meister',
 		'spengler-cup-winner': 'Spengler-Cup-Sieger',
+		'hobey-baker-memorial-award-finalist': 'Finalist Hobey-Baker-Award',
 		'1994': '1994/95 aktive Spieler',
 		'2025': '2025/26 aktive Spieler',
 		'WJC-20': 'U20-WM-Teilnehmer',
 		'WJC-20_MEDAL': 'U20-WM-Medaille',
-		'WJC-20_GOLD_MEDAL': 'U20-WM-Titel',
+		'u20-wjc-gold-medal': 'U20-WM-Titel',
+		'world-championship-medal': 'WM-Medaille',
+		'shl-sm-silver-medal': 'SHL-Vizemeister',
 		'drafted': 'Ausgewählt im NHL-Draft',
-		'not_drafted': 'Nicht ausgewählt im NHL-Draft'
+		'not_drafted': 'Nicht ausgewählt im NHL-Draft',
+		'dual_nationalities': 'Doppelflaggen-Spieler',
+		'draft_2000s': 'Ausgewählt im NHL-Draft der 2000er',
+		'draft_round_1': 'In Runde 1 gedraftet',
+		'draft_round_2': 'In Runde 2 gedraftet',
+		'goalie': 'Torwart',
+		'defender': 'Verteidiger',
+		'forward': 'Stürmer',
+		'goalscorer_over_40': 'Torschütze über 40 Jahre',
+		'relative_del': 'Verwandter mit DEL-Spiel(en)',
+		'relative_nhl': 'Verwandter mit NHL-Spiel(en)',
+		'50_w_career': '50 Siege / Karriere',
+		'10_w_season_po': '10 Siege in Playoffs / Saison',
+		'le_3.0_gaa_season_po': 'Saison mit ≤ 3,0 GTS in Playoffs'
 	},
 	en: {
 		'20_goal': '20 Goals / Season',
-		'30_assist': '30 Assists / Season',
 		'50_point': '50 Points / Season',
 		'20_g_season': '20 Goals / Season',
+		'30_g_season': '30 Goals / Season',
+		'5_g_season_po': '5 Goals / Season (Playoffs)',
+		'30_a_season': '30 Assists / Season',
 		'20_pts_season': '20 Points / Season',
 		'40_pts_season': '40 Points / Season',
 		'50_pts_season': '50 Points / Season',
@@ -148,33 +225,68 @@ export const categories: Record<Language, Record<CategoryKey, string>> = {
 		'70_pts_season': '70 Points / Season',
 		'100_pim_season': '100 PIMs / Season',
 		'1_so_season': '1+ Shutout Season',
+		'1_so_season_po': '1+ Shutout Season (Playoffs)',
+		'le_2.5_gaa_season': '≤ 2.50 GAA / Season',
 		'100_gp_career': '100 Games / Career',
 		'500_gp_career': '500 Games / Career',
+		'10_gp_career_po': '10 Playoff Games / Career',
+		'25_gp_career_po': '25 Playoff Games / Career',
+		'50_gp_career_po': '50 Playoff Games / Career',
+		'100_gp_career_po': '100 Playoff Games / Career',
+		'50_g_career': '50 Goals / Career',
 		'100_g_career': '100 Goals / Career',
 		'150_g_career': '150 Goals / Career',
 		'200_g_career': '200 Goals / Career',
+		'10_g_career_po': '10 Playoff Goals / Career',
 		'100_a_career': '100 Assists / Career',
 		'200_a_career': '200 Assists / Career',
 		'250_a_career': '250 Assists / Career',
 		'250_pts_career': '250 Points / Career',
 		'400_pts_career': '400 Points / Career',
 		'500_pts_career': '500 Points / Career',
+		'30_pts_career_po': '30 Playoff Points / Career',
 		'500_pim_career': '500 PIMs / Career',
+		'100_pim_career_po': '100 PIMs in Playoffs / Career',
+		'50_pim_career_po': '50 PIMs in Playoffs / Career',
 		'10_so_career': '10+ Shutouts / Career',
+		'25_so_career': '25+ Shutouts / Career',
+		'5_so_season': '5+ Shutout Season',
+		'le_100_gp_career': '≤ 100 Games / Career',
+		'le_10_gp_career': '≤ 10 Games / Career',
 		'1_teams': 'Single Team',
 		'2_teams': 'Exactly 2 Teams',
 		'3_plus_teams': '3+ Teams',
+		'3_teams': 'Exactly 3 Teams',
 		'5_plus_teams': '5+ Teams',
+		'6_teams': 'Exactly 6 Teams',
 		'del-champion': 'DEL Champion',
+		'del2-champion': 'DEL2 Champion',
+		'dnl-champion': 'DNL Champion',
 		'italy-champion': 'Italian Champion',
 		'spengler-cup-winner': 'Spengler Cup Champion',
+		'hobey-baker-memorial-award-finalist': 'Hobey Baker Award Finalist',
+		'world-championship-medal': 'World Championship Medal',
+		'shl-sm-silver-medal': 'SHL Silver Medal',
 		'1994': '1994/95 Active Players',
 		'2025': '2025/26 Active Players',
 		'WJC-20': 'U20s Participant',
 		'WJC-20_MEDAL': 'U20s Medal',
-		'WJC-20_GOLD_MEDAL': 'U20s Champion',
+		'u20-wjc-gold-medal': 'U20s Gold Medal',
 		'drafted': 'Selected in NHL Draft',
-		'not_drafted': 'Not selected in NHL Draft'
+		'not_drafted': 'Not selected in NHL Draft',
+		'draft_round_1': 'Drafted in Round 1',
+		'draft_round_2': 'Drafted in Round 2',
+		'dual_nationalities': 'Dual Nationalities',
+		'draft_2000s': 'Selected in NHL Draft of 2000s',
+		'goalie': 'Goalie',
+		'defender': 'Defender',
+		'forward': 'Forward',
+		'goalscorer_over_40': 'Goalscorer over 40 years',
+		'relative_del': 'Relative with DEL game(s) played',
+		'relative_nhl': 'Relative with NHL game(s) played',
+		'50_w_career': '50 Wins / Career',
+		'10_w_season_po': '10 Wins in Playoffs / Season',
+		'le_3.0_gaa_season_po': '≤ 3.0 GAA / Season in Playoffs'
 	}
 };
 
